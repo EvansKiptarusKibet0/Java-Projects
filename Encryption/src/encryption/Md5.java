294b8f43843dac5b1d12743177f791b368711c45
Evans Kiptarus Kibet <evanskibet114@gmail.com>
2017-12-10 23:36:05 +0300

    Add files via upload

---
+++ Encryption/src/encryption/Md5.java
@@ -0,0 +1,19 @@
+package encryption;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class Md5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//public static String encrypMd5(String data){
+	//MessageDigest md=MessageDigest.getInstance("Md5");
+	//byte[] messageDigest=md.digest(data.getBytes());
+	//BigInteger num=new BigInteger(1,messageDigest);
+	//String hashtext=num.toString();
+	
+}
+	}
+
+//}
